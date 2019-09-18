3f49d7add1f8df358ed3203fda3541d307323ccd
marcio lemos <contato.marciovrl@gmail.com>
2019-09-18 14:40:54 -0300

    fix formatting

--- README.md
+++ README.md
@@ -1,58 +1,29 @@
 # example-protractor
+
 Example of using the protractor for automated e2e testing.
 
-# Pré-condições:
-- Node instalado;
-- NPM instalado;
-- Chromedriver instalado.
+# Preconditions:
 
-# Começar projeto do zero:
-- Primeiro iniciar um projeto node:
-```
-npm init
-```
+- Node installed;
+- NPM installed;
+- Chromedriver installed.
 
-- Buscar as dependencias necessarias ao seu projeto em https://www.npmjs.com/ e adiciona-los no arquivo `package.json`.
+# Use this project as a template:
 
-- Instalar dependencias que foram adicionadas no projeto:
-```
-npm install
-```
+- Clone the project:
 
-- Configurar arquivo de execução do Protractor. Exemplo `protractor.conf.js`.
-
-- Executar testes `protractor protractor.conf.js`.
-
-# Usar esse projeto como modelo:
-- Clonar o projeto:
 ```
 git clone https://github.com/marciovrl/example-protractor.git
 ```
 
-- 
+- Install dependencies
+
 ```
 npm install
 ```
 
--
+- Run the testes
+
 ```
 npm run test
 ```
-
-# Estrutura do projeto
-```
-|
-| - features
-    | - pages
-    | - specifications
-    | - step_definitions
-    | - support
-| - .gitignore
-| - protractor.conf.js
-| - package.json
-| - README.md 
-```
-
-
-
-
--- features/support/helper.js
+++ features/support/helper.js
@@ -1,31 +1,46 @@
-let until = protractor.ExpectedConditions
-let fs = require('fs')
-let Buffer = require('safe-buffer').Buffer
+let until = protractor.ExpectedConditions;
+let fs = require("fs");
+let Buffer = require("safe-buffer").Buffer;
 
-const { setDefaultTimeout } = require('cucumber')
-setDefaultTimeout(60 * 1000)
+const { setDefaultTimeout } = require("cucumber");
+setDefaultTimeout(60 * 1000);
 
-let Helper = function () {}
+let Helper = function() {};
 
-Helper.prototype.isElementAttachedToHtml = function (element) {
-    browser.wait(until.presenceOf(element), 25000, 'Element ' + element.getText() + ' taking too long to appear in the DOM')
-    browser.executeScript('arguments[0].scrollIntoView();', element.getWebElement())
-}
+Helper.prototype.isElementAttachedToHtml = function(element) {
+  browser.wait(
+    until.presenceOf(element),
+    25000,
+    "Element " + element.getText() + " taking too long to appear in the DOM"
+  );
+  browser.executeScript(
+    "arguments[0].scrollIntoView();",
+    element.getWebElement()
+  );
+};
 
-Helper.prototype.isClickable = function (element) {
-    browser.wait(until.elementToBeClickable(element), 50000, 'Element taking too long to appear in the DOM and stay clickable')
-}
+Helper.prototype.isClickable = function(element) {
+  browser.wait(
+    until.elementToBeClickable(element),
+    50000,
+    "Element taking too long to appear in the DOM and stay clickable"
+  );
+};
 
-Helper.prototype.isVisible = function (element) {
-    browser.wait(until.visibilityOf(element), 10000, 'Element taking too long to appear in the DOM and stay visible')
-}
+Helper.prototype.isVisible = function(element) {
+  browser.wait(
+    until.visibilityOf(element),
+    10000,
+    "Element taking too long to appear in the DOM and stay visible"
+  );
+};
 
-Helper.prototype.stopBrowser = function (time) {
-    browser.sleep(time)
-}
+Helper.prototype.stopBrowser = function(time) {
+  browser.sleep(time);
+};
 
-Helper.prototype.scrollPageDown = function (valuePixels) {
-  browser.executeScript('window.scrollBy(0,' + valuePixels + ');')
-}
+Helper.prototype.scrollPageDown = function(valuePixels) {
+  browser.executeScript("window.scrollBy(0," + valuePixels + ");");
+};
 
-module.exports = Helper
+module.exports = Helper;
