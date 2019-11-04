7cdb1c32e6a4de23103405d750cad227f1a7fc33
marcio lemos <contato.marciovrl@gmail.com>
2019-11-04 13:49:42 -0300

    add search_item steps

---
+++ features/step_definitions/search_item.js
@@ -0,0 +1,11 @@
+Given("that it is on the homepage of Mercado Livre", function() {
+  // Write code here that turns the phrase above into concrete actions
+});
+
+When("I search for {string}", function(string) {
+  // Write code here that turns the phrase above into concrete actions
+});
+
+Then("I view items according to my search", function() {
+  // Write code here that turns the phrase above into concrete actions
+});
